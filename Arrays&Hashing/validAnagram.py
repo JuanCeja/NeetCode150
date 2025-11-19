1d8bb4ab9aa8aaac23f64a7af0fd176c449d95d7
Juan Ceja MBP <jmc347ls2@yahoo.com>
2025-11-18 18:19:39 -0800

    initial commit

---
+++ Arrays&Hashing/validAnagram.py
@@ -0,0 +1,20 @@
+# Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+# An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+# Example 1:
+
+# Input: s = "racecar", t = "carrace"
+
+# Output: true
+# Example 2:
+
+# Input: s = "jar", t = "jam"
+
+# Output: false
+
+def is_anagram(s: str, t: str) -> bool:
+    return True
+
+print(is_anagram("racecar", "carrace"))
+print(is_anagram("jar", "jam"))
