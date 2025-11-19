c2eeaab292797e6d86a537a959a080aca47e6195
Juan Ceja MBP <jmc347ls2@yahoo.com>
2025-11-18 18:59:37 -0800

    create working solution

--- Arrays&Hashing/validAnagram.py
+++ Arrays&Hashing/validAnagram.py
@@ -14,7 +14,32 @@
 # Output: false
 
 def is_anagram(s: str, t: str) -> bool:
+    """"This function checks if both strings are anagrams of each other."""
+    if len(s) != len(t): return False
+
+    s_counter = {}
+    t_counter = {}
+
+    for i in range(len(s)):
+        if s[i] in s_counter:
+            s_counter[s[i]] = s_counter[s[i]] + 1
+        else: s_counter[s[i]] = 1
+
+    for i in range(len(t)):
+        if t[i] in t_counter:
+            t_counter[t[i]] = t_counter[t[i]] + 1
+        else: t_counter[t[i]] = 1
+
+
+    for key in s_counter:
+        if key not in t_counter:
+            return False
+        if s_counter[key] != t_counter[key]:
+            return False
+
     return True
+
+
 
 print(is_anagram("racecar", "carrace"))
 print(is_anagram("jar", "jam"))
