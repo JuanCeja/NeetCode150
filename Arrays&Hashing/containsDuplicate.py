70db301ed2c8e994c6e2f164a62c0f8c0b7a68f5
Juan Ceja MBP <jmc347ls2@yahoo.com>
2025-11-18 17:36:42 -0800

    initial commit

---
+++ Arrays&Hashing/containsDuplicate.py
@@ -0,0 +1,20 @@
+# Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+# Example 1:
+
+# Input: nums = [1, 2, 3, 3]
+
+# Output: true
+
+# Example 2:
+
+# Input: nums = [1, 2, 3, 4]
+
+# Output: false
+
+def has_duplicate(nums: List[int]) -> bool:
+    return True;
+
+
+print(has_duplicate([1, 2, 3, 3]));
+print(has_duplicate([1, 2, 3, 4]));
