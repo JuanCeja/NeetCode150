d6f0c831d144ca6fc4ed19e2a4f66531505034d2
Juan Ceja MBP <jmc347ls2@yahoo.com>
2025-11-19 18:01:45 -0800

    create pseudo code or ideal plan for algorithm

--- Arrays&Hashing/groupAnagrams.py
+++ Arrays&Hashing/groupAnagrams.py
@@ -20,7 +20,26 @@
 # Output: [[""]]
 
 def group_anagrams(strs: list[str]) -> list[list[str]]:
-    return []
+    # if len < 2 return strs
+
+    # array we will be returning
+
+    # create an array with 26 0's
+
+    # outer for loop
+
+        # inner for loop
+
+        # create unique array and set it as a value
+
+        # check if unique array exists in hashmap
+
+            # if so push current word to it
+
+        # if not, create unique array as key and current word being the value
+
+    # return array
+
 
 print(group_anagrams(["act","pots","tops","cat","stop","hat"]))
 print(group_anagrams(["x"]))
