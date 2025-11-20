0c3ed206c5fc598d086e1d9f20911fca3ba20722
Juan Ceja MBP <jmc347ls2@yahoo.com>
2025-11-19 18:27:23 -0800

    create a working solution

--- Arrays&Hashing/groupAnagrams.py
+++ Arrays&Hashing/groupAnagrams.py
@@ -20,25 +20,41 @@
 # Output: [[""]]
 
 def group_anagrams(strs: list[str]) -> list[list[str]]:
-    # if len < 2 return strs
 
     # array we will be returning
+    grouped_anagrams = []
+    hashmap = {}
 
     # create an array with 26 0's
+    
+
 
     # outer for loop
-
+    for current_str in strs:
+        arr = [0] * 26
         # inner for loop
-
+        for char in current_str:
         # create unique array and set it as a value
+            letter_index = ord(char) - ord('a')
+            arr[letter_index] += 1
+
 
         # check if unique array exists in hashmap
-
+        my_tuple = tuple(arr)
+        
+        if my_tuple in hashmap:
             # if so push current word to it
+            hashmap[my_tuple].append(current_str)
+        else:
+            # if not, create unique array as key and current word being the value
+            hashmap[my_tuple] = [current_str]
 
-        # if not, create unique array as key and current word being the value
+
+    for value in hashmap.values():
+        grouped_anagrams.append(value)
 
     # return array
+    return grouped_anagrams
 
 
 print(group_anagrams(["act","pots","tops","cat","stop","hat"]))
