5cf74183d82ccb48f87ebc26fcb3985ce77171fe
Juan Ceja MBP <jmc347ls2@yahoo.com>
2025-11-19 20:30:03 -0800

    creaet working solution

--- topKFrequentElements.py
+++ topKFrequentElements.py
@@ -16,7 +16,21 @@
 # Output: [7]
 
 def top_k_frequent(nums: list[int], k: int) -> list[int]:
-    return []
+    frequency_counter = {}
+    frequent_elements = []
+
+    for num in nums:
+        if num in frequency_counter:
+            frequency_counter[num] += 1
+        else: frequency_counter[num] = 1
+
+    sorted_items = sorted(frequency_counter.items(), key = lambda x: x[1])
+
+    beggining = len(sorted_items) - k
+    for i in sorted_items[beggining:len(sorted_items)]:
+        frequent_elements.append(i[0])
+
+    return frequent_elements
 
 print(top_k_frequent([1,2,2,3,3,3], 2))
 print(top_k_frequent([7, 7], 1))
