4ee57002cd5ef5c57d8d736dbaa70f70db430482
Juan Ceja MBP <jmc347ls2@yahoo.com>
2025-11-25 16:51:23 -0800

    added docstring to solution

--- topKFrequentElements.py
+++ topKFrequentElements.py
@@ -16,6 +16,7 @@
 # Output: [7]
 
 def top_k_frequent(nums: list[int], k: int) -> list[int]:
+    """Return the k most frequent elements from the array."""
     frequency_counter = {}
 
     for num in nums:
