8a59998241b2a10586420c518bf19ca7baf63ef0
JuanCeja <jmc347ls2@yahoo.com>
2025-12-08 16:56:50 -0800

    clean up solution

--- Two_Pointers/medium/container_with_most_water.py
+++ Two_Pointers/medium/container_with_most_water.py
@@ -12,20 +12,21 @@
 # Output: 4
 
 def max_area(heights: list[int]) -> int:
-    # max area var
-    # left and right
-    # width of area
+    max_area = 0
+    left, right = 0, len(heights) - 1
 
-    # while l < r
-        # calculate current area
+    while left < right:
+        width = right - left
 
-        # update max area if current is >
+        current_area = min(heights[left], heights[right]) * width
+        max_area = max(current_area, max_area)
 
-        # move move smaller gate accordingly
-
-    # return our max area
-
-    return 0
+        if heights[left] < heights[right]:
+            left += 1
+        else:
+            right -= 1
+            
+    return max_area
 
 print(max_area([1,7,2,5,4,7,3,6])) # 36
 print(max_area([2,2,2])) # 4
