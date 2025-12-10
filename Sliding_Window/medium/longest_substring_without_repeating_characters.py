1698832215b7eb1db4cf473ee3a11fcc625dbef2
JuanCeja <jmc347ls2@yahoo.com>
2025-12-09 17:31:17 -0800

    create an attempt at an efficient working solution

--- Sliding_Window/medium/longest_substring_without_repeating_characters.py
+++ Sliding_Window/medium/longest_substring_without_repeating_characters.py
@@ -5,25 +5,22 @@
 # 
 
 def length_of_longest_substring(s: str) -> int:
-    # pointers
+    left, right = 0, 1    
+    counter = 0
+    substring_set = set()
+    substring_set.add(s[left])
 
-    # counter
-
-    # set with l already 
-
-    # while r < len(s):
-
-        # if r not in set:
-
-            # counter ++
-
-        # else 
-
-            # left = right + 1
-            # right += 1
-            # counter = 1
-
-    # return counter
+    while right < len(s):
+        if s[right] not in substring_set:
+            counter += 1
+            right += 1
+        else:
+            left = right + 1
+            right += 1
+            counter = 1
+            substring_set = set()
+            
+    return counter
 
 print(length_of_longest_substring("zxyzxyz")) # 3
 print(length_of_longest_substring("xxxx")) # 1
