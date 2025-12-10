38b914f6936aee45a6a14511624ad322e1f009a4
JuanCeja <jmc347ls2@yahoo.com>
2025-12-09 19:35:44 -0800

    initial commit for longest repeating characater replacement algorithm

---
+++ Sliding_Window/medium/longest_repeating_character_replacement.py
@@ -0,0 +1,13 @@
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+
+def character_replacement(s: str, k: int) -> int:
+    return 0
+
+print(character_replacement("XYYX", 2)) # 4
+print(character_replacement("AAABABB", 1)) # 5
