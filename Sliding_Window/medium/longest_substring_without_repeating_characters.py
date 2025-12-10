52d12f6bbc334a763fab8abda788d5fb258dd966
JuanCeja <jmc347ls2@yahoo.com>
2025-12-09 16:44:06 -0800

    initial commit for longest substring without repeating characters algo

---
+++ Sliding_Window/medium/longest_substring_without_repeating_characters.py
@@ -0,0 +1,11 @@
+# 
+# 
+# 
+# 
+# 
+
+def length_of_longest_substring(s: str) -> int:
+    return 0
+
+print(length_of_longest_substring("zxyzxyz")) # 3
+print(length_of_longest_substring("xxxx")) # 1
