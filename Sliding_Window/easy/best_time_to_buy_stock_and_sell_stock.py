8891d8a2a963f212e85efdd98068d3c5b47db57e
JuanCeja <jmc347ls2@yahoo.com>
2025-12-09 16:26:26 -0800

    approach for best time to buy and sell stock algo

--- Sliding_Window/easy/best_time_to_buy_stock_and_sell_stock.py
+++ Sliding_Window/easy/best_time_to_buy_stock_and_sell_stock.py
@@ -4,6 +4,19 @@
 # 
 
 def max_profit (prices: list[int]) -> int:
+    # max profit
+    # pointers
+
+    # while right < len prices
+
+        # calculate profit and assign
+
+        # if left < move left up
+
+        # move right up
+
+    # return max profit
+
     return 0
 
 print(max_profit([10,1,5,6,7,1])) # 6
