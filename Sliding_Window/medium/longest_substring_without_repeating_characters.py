412293c1c92536fb82e1847b0f37c7b4123e3a02
JuanCeja <jmc347ls2@yahoo.com>
2025-12-09 17:24:20 -0800

    create pseudo code approach for longest substring without repeating characters

--- Sliding_Window/medium/longest_substring_without_repeating_characters.py
+++ Sliding_Window/medium/longest_substring_without_repeating_characters.py
@@ -5,7 +5,25 @@
 # 
 
 def length_of_longest_substring(s: str) -> int:
-    return 0
+    # pointers
+
+    # counter
+
+    # set with l already 
+
+    # while r < len(s):
+
+        # if r not in set:
+
+            # counter ++
+
+        # else 
+
+            # left = right + 1
+            # right += 1
+            # counter = 1
+
+    # return counter
 
 print(length_of_longest_substring("zxyzxyz")) # 3
 print(length_of_longest_substring("xxxx")) # 1
