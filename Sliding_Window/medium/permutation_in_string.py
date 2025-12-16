865b1a3057a2444adb3b69b309bbe5e4f25be961
JuanCeja <jmc347ls2@yahoo.com>
2025-12-15 19:17:55 -0800

    pseudo code/approach plan for permutation in string algorithm

--- Sliding_Window/medium/permutation_in_string.py
+++ Sliding_Window/medium/permutation_in_string.py
@@ -9,8 +9,16 @@
 
 
 def permutation_in_string(s1: str, s2: str) -> bool:
-    
-    return True
+    # create freq counter s1
+    # create freq counter s2
+
+    # for loop fq s1 != fq s2
+
+        # make comparison # if so return true
+
+        # else slide window up
+
+    return False
 
 print(permutation_in_string("abc", "lecabee")) # True
 print(permutation_in_string("abc", "lecaabee")) # False
