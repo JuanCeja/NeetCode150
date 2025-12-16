cc93d07d92cefa36fabfdb22a99bfd0589119cdd
JuanCeja <jmc347ls2@yahoo.com>
2025-12-15 18:31:46 -0800

    initial commit for permutation in string

---
+++ Sliding_Window/medium/permutation_in_string.py
@@ -0,0 +1,16 @@
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+
+
+def permutation_in_string(s1: str, s2: str) -> bool:
+    
+    return True
+
+print(permutation_in_string("abc", "lecabee")) # True
+print(permutation_in_string("abc", "lecaabee")) # False
