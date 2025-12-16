a92f6e2e14b518c2d845b4c9fe39a6895d41a0c1
JuanCeja <jmc347ls2@yahoo.com>
2025-12-15 19:46:37 -0800

    create working solution with time complexity of O(n)

--- Sliding_Window/medium/permutation_in_string.py
+++ Sliding_Window/medium/permutation_in_string.py
@@ -9,14 +9,31 @@
 
 
 def permutation_in_string(s1: str, s2: str) -> bool:
-    # create freq counter s1
-    # create freq counter s2
+    if len(s1) > len(s2):
+        return False
+    
+    frequency_counter_s1 = {}
+    frequency_counter_s2 = {}
+    s1_len = len(s1)
+    window_size = len(s1)
+    
+    for i in range(s1_len):
+        frequency_counter_s1[s1[i]] = frequency_counter_s1.get(s1[i], 0) + 1
+        frequency_counter_s2[s2[i]] = frequency_counter_s2.get(s2[i], 0) + 1
 
-    # for loop fq s1 != fq s2
+    if frequency_counter_s1 == frequency_counter_s2:
+        return True
 
-        # make comparison # if so return true
+    for j in range(window_size, len(s2)):
+        if s2[j - window_size] in frequency_counter_s2:
+            frequency_counter_s2[s2[j - window_size]] -= 1
+            if frequency_counter_s2[s2[j - window_size]] == 0:
+                del frequency_counter_s2[s2[j - window_size]]
+                
+        frequency_counter_s2[s2[j]] = frequency_counter_s2.get(s2[j], 0) + 1
 
-        # else slide window up
+        if frequency_counter_s1 == frequency_counter_s2:
+            return True
 
     return False
 
