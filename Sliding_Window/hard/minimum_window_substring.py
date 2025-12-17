296c959b888e533f1f38c9080d43ae58d5bb30e9
JuanCeja <jmc347ls2@yahoo.com>
2025-12-16 16:28:25 -0800

    first attempt for minimum window substring algorithm

--- Sliding_Window/hard/minimum_window_substring.py
+++ Sliding_Window/hard/minimum_window_substring.py
@@ -8,7 +8,33 @@
 # 
 
 def min_window_substring(s: str, t: str) -> str:
-    return ""
+    if len(t) > len(s):
+        return ""
+    
+    left = 0
+    t_count = {}
+    window_count = {}
+    
+    for i in range(len(t)):
+        t_count[t[i]] = t_count.get(t[i], 0) + 1
+        window_count[s[i]] = window_count.get(s[i], 0) + 1
+        
+    if t_count == window_count:
+        return s[0:len(t)]
+        
+    for i in range(len(t), len(s)):
+        if s[i] in t_count:
+            t_count[t[i]] = t_count.get(t[i]) - 1
+            if t_count[t[i]] == 0:
+                del t_count[t[i]]
+                
+        window_count[s[i]] = window_count.get(s[i], 0) + 1
+        
+        if not t_count:
+            while s[left] not in t:
+                left += 1
+    
+    return s[left:i]
 
 print(min_window_substring("OUZODYXAZV", "XYZ")) # "YXAZ"
 print(min_window_substring("xyz","xyz")) # "xyz"
