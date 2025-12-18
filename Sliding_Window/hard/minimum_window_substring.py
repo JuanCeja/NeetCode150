2c3b4f833ef32d853bc37d5dd845bc8c437fd6a6
JuanCeja <jmc347ls2@yahoo.com>
2025-12-17 19:09:13 -0800

    create optimal working solution for minimum window substring

--- Sliding_Window/hard/minimum_window_substring.py
+++ Sliding_Window/hard/minimum_window_substring.py
@@ -8,37 +8,41 @@
 # 
 
 def min_window_substring(s: str, t: str) -> str:
-    if len(t) > len(s):
+    if len(s) < len(t):
         return ""
     
     t_count, window = {}, {}
+    
+    for c in t:
+        t_count[c] = t_count.get(c, 0) + 1
+        
     have, need = 0, len(t_count)
     res, res_len = "", float("inf")
     left = 0
         
-    for c in t:
-        t_count[c] = t_count.get(c, 0) + 1
-        
     for right in range(len(s)):
+        
         char = s[right]
         window[char] = window.get(char, 0) + 1
         
-        if window[char] in t_count and window[char] == t_count[char]:
-            have =+ 1
+        if char in t_count and window[char] == t_count[char]:
+            have += 1
             
         while have == need:
-            window[left] -= 1
-            left += 1
+            if right - left + 1 < res_len:
+                res = s[left:right + 1]
+                res_len = right - left + 1
             
-            if window[char] < t_count[char]:
+            removed_char = s[left]
+            window[removed_char] -= 1
+            
+            if removed_char in t_count and window[removed_char] < t_count[removed_char]:
                 have -= 1
                 
-            if (right - left + 1) < res_len:
-                res = s[left:right]
-                res_len = right - left + 1
+            left += 1
                 
     return res if res_len < float("inf") else ""
 
 print(min_window_substring("OUZODYXAZV", "XYZ")) # "YXAZ"
 print(min_window_substring("xyz","xyz")) # "xyz"
-print(min_window_substring("x", "xy")) # "xy"
+print(min_window_substring("x", "xy")) # ""
