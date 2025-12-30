7ffb590d8322e66b95591f9f743821d97d94a3fd
JuanCeja <jmc347ls2@yahoo.com>
2025-12-29 23:00:46 -0800

    create pseudo code for brute force solution

--- Sliding_Window/hard/sliding_window_maximum.py
+++ Sliding_Window/hard/sliding_window_maximum.py
@@ -9,6 +9,19 @@ from collections import deque
 
 def max_sliding_window(nums: list[int], k: int) -> list[int]:
     
+    # res 
+    
+    # outer for loop
+    
+        # inner for loop
+        
+            # max var = i
+            
+            # max var = max(i, j)
+            
+            # if in last iteration push max
+            
+    # return res
 
 
 print(max_sliding_window([1,2,1,0,4,2,6], 3)) # [2,2,4,4,6]
