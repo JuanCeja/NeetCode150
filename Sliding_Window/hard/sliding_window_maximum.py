e5b7f47eed8c472116cb4d2b1a7a23e488b40c44
JuanCeja <jmc347ls2@yahoo.com>
2025-12-29 22:54:45 -0800

    initial commit for sliding window maximum algo

--- Sliding_Window/hard/sliding_window_maximum.py
+++ Sliding_Window/hard/sliding_window_maximum.py
@@ -8,24 +8,7 @@ from collections import deque
 
 
 def max_sliding_window(nums: list[int], k: int) -> list[int]:
-    output = []
-    q = deque()
-    l = r = 0
     
-    while r < len(nums):
-        while q and nums[q[-1]] < nums[r]:
-            q.pop()
-        q.append(r)
-        
-        if l > q[0]:
-            q.popleft()
-            
-        if (r + 1) >= k:
-            output.append(nums[q[0]])
-            l += 1
-        r += 1
-    
-    return output
 
 
 print(max_sliding_window([1,2,1,0,4,2,6], 3)) # [2,2,4,4,6]
