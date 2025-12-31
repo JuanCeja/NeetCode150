24d70b7371c1f7787cfc2de31d78e66882b2a5e4
JuanCeja <jmc347ls2@yahoo.com>
2025-12-30 21:53:12 -0800

    initial commit for valid parenthesis algo

---
+++ stack/easy/valid_parentheses.py
@@ -0,0 +1,16 @@
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+
+def is_valid(s: str) -> bool:
+    
+    return True
+
+print(is_valid("[]")) # true
+print(is_valid("([{}])")) # true
+print(is_valid("[(])")) # false
