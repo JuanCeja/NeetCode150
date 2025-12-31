4bfed6eebaf3ea244e291a8bda83d32c2f2ee593
JuanCeja <jmc347ls2@yahoo.com>
2025-12-30 22:02:47 -0800

    plan / pseudo code for valid parenthesis algo in linear time

--- stack/easy/valid_parentheses.py
+++ stack/easy/valid_parentheses.py
@@ -8,6 +8,21 @@
 # 
 
 def is_valid(s: str) -> bool:
+    # stack
+    
+    # iterate s
+    
+        # if an opening bracket
+        
+            # push to stack
+            
+        # if ) and stack -1 is ( pop
+        
+        # do the same for the rest
+        
+    
+    # return stack empty = true else false
+    
     
     return True
 
