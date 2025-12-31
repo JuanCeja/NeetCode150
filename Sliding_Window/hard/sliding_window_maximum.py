b4a51aa68310d880f52e3c8b2818049cf888f92f
JuanCeja <jmc347ls2@yahoo.com>
2025-12-30 21:29:20 -0800

    first attempt at creating solution with a linear time complexity

--- Sliding_Window/hard/sliding_window_maximum.py
+++ Sliding_Window/hard/sliding_window_maximum.py
@@ -6,28 +6,26 @@
 
 
 
-def max_sliding_window(nums: list[int], k: int) -> list[int]:
-    # res 
-    
-    # dq
-    
-    # iterate nums
-    
-        # while left most in dq is not in window keep removing from front
-        
-        
-        # while curr num is > than last of dq keep popping
-        
-        
-        # add our current num to dq after we got rid of what we dont need 
-        
+from collections import deque
 
-        # now check if window is full
-        
-            # if so get the max and append it to res
-            
-            
-    # return res
+
+def max_sliding_window(nums: list[int], k: int) -> list[int]:
+    res = []
+    dq = deque()
     
+    for i in range(len(nums)):
+        
+        while dq and dq[0] < i - k + 1:
+            dq.popleft()
+        
+        while dq and nums[dq[-1]] < nums[i]:
+            dq.pop()
+            
+        dq.append(i)
+        
+        if i - k + 1 > k:
+            res.append(nums[dq[0]])
+            
+    return res
 
 print(max_sliding_window([1,2,1,0,4,2,6], 3)) # [2,2,4,4,6]
