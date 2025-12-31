d7f94f346298a1915ab5b864b6f61362c3a78234
JuanCeja <jmc347ls2@yahoo.com>
2025-12-30 22:07:40 -0800

    first attempt

--- stack/easy/valid_parentheses.py
+++ stack/easy/valid_parentheses.py
@@ -9,19 +9,27 @@
 
 def is_valid(s: str) -> bool:
     # stack
-    
+    stack = []
     # iterate s
-    
+    for char in s:
         # if an opening bracket
-        
+        if char == "(" or char == "{" or char == "[":
             # push to stack
-            
+            stack.append(s)
+            continue
         # if ) and stack -1 is ( pop
-        
-        # do the same for the rest
+        if char == ")" and stack[-1] == "(":
+            stack.pop()
+
+        if char == "}" and stack[-1] == "{":
+            stack.pop()
+
+        if char == "]" and stack[-1] == "[":
+            stack.pop()
         
     
     # return stack empty = true else false
+    return True if len(stack) == 0 else False
     
     
     return True
