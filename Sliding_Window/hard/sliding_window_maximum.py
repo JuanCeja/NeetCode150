0370d3f250e26fc7f388059df53638ce00f84778
JuanCeja <jmc347ls2@yahoo.com>
2025-12-30 19:07:11 -0800

    create plan / pseudo code for a optimized solution for algo sliding window maximum

--- Sliding_Window/hard/sliding_window_maximum.py
+++ Sliding_Window/hard/sliding_window_maximum.py
@@ -7,14 +7,27 @@
 
 
 def max_sliding_window(nums: list[int], k: int) -> list[int]:
+    # res 
     
-    res = []
+    # dq
     
-    for index, value in enumerate(nums[:len(nums) - k + 1]):
-        window = nums[index: index + k]
-        max_value = max(window)
-        res.append(max_value)
+    # iterate nums
+    
+        # while left most in dq is not in window keep removing from front
         
-    return res
+        
+        # while curr num is > than last of dq keep popping
+        
+        
+        # add our current num to dq after we got rid of what we dont need 
+        
+
+        # now check if window is full
+        
+            # if so get the max and append it to res
+            
+            
+    # return res
+    
 
 print(max_sliding_window([1,2,1,0,4,2,6], 3)) # [2,2,4,4,6]
