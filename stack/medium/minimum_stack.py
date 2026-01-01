16c41e8543794ecf2bc78465d934a8d7f48e29ba
JuanCeja <jmc347ls2@yahoo.com>
2025-12-31 16:18:44 -0800

    initial commit for minimum stack algo

---
+++ stack/medium/minimum_stack.py
@@ -0,0 +1,28 @@
+# 
+# 
+# 
+
+class MinStack:
+
+    def __init__(self):
+        
+
+    def push(self, val: int) -> None:
+        
+
+    def pop(self) -> None:
+        
+
+    def top(self) -> int:
+        
+
+    def getMin(self) -> int:
+
+MinStack minStack = new MinStack();
+minStack.push(1)
+minStack.push(2)
+minStack.push(0)
+print(minStack.getMin()) # return 0
+minStack.pop()
+print(minStack.top())    # return 2
+print(minStack.getMin()) # return 1
