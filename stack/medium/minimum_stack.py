516178dec18e3ff3bab813c49e4e4eedba7ae33a
JuanCeja <jmc347ls2@yahoo.com>
2025-12-31 16:55:36 -0800

    finish creating all methods for the MinStack class

--- stack/medium/minimum_stack.py
+++ stack/medium/minimum_stack.py
@@ -5,20 +5,26 @@
 class MinStack:
 
     def __init__(self):
-        
+        self.stack = []
 
     def push(self, val: int) -> None:
-        
+        self.stack.append(val)
 
     def pop(self) -> None:
-        
+        self.stack.pop()
 
     def top(self) -> int:
-        
+        return self.stack[-1]
 
     def getMin(self) -> int:
+        lowest_num = float("inf")
+        
+        for num in self.stack:
+            lowest_num = min(lowest_num, num)
+            
+        return lowest_num
 
-MinStack minStack = new MinStack();
+minStack = MinStack()
 minStack.push(1)
 minStack.push(2)
 minStack.push(0)
