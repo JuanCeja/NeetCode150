ffc81218ad5811a5447121dc8440234613789037
JuanCeja <jmc347ls2@yahoo.com>
2026-01-01 19:01:59 -0800

    initial commit for daily temperature algorithm

---
+++ stack/medium/daily_temperature.py
@@ -0,0 +1,10 @@
+# 
+# 
+# 
+
+
+def daily_temperature(temperatures: list[int]) -> list[int]:
+    
+    
+print(daily_temperature([30,38,30,36,35,40,28])) # [1,4,1,2,1,0,0]
+print(daily_temperature([22,21,20])) # [0,0,0]
