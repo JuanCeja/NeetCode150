bf6f55b07bc39a5840d47fa5bb01f8c4d922a6f5
JuanCeja <jmc347ls2@yahoo.com>
2026-01-01 21:16:22 -0800

    create refactored solution in linear time

--- stack/medium/daily_temperature.py
+++ stack/medium/daily_temperature.py
@@ -4,7 +4,18 @@
 
 
 def daily_temperature(temperatures: list[int]) -> list[int]:
+    result = [0] * len(temperatures)
+    stack = []
     
-    
+    for i in range(len(temperatures)):
+        
+        while stack and temperatures[i] > temperatures[stack[-1]]:
+            result[stack[-1]] = i - stack[-1]
+            stack.pop()
+            
+        stack.append(i)
+        
+    return result
+
 print(daily_temperature([30,38,30,36,35,40,28])) # [1,4,1,2,1,0,0]
 print(daily_temperature([22,21,20])) # [0,0,0]
