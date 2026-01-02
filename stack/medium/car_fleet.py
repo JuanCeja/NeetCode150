c17aba581d369f8d2a0a3763cdddf3c76319382d
JuanCeja <jmc347ls2@yahoo.com>
2026-01-01 23:16:47 -0800

    create pseudo code / plan for car fleet algorithm

--- stack/medium/car_fleet.py
+++ stack/medium/car_fleet.py
@@ -4,7 +4,20 @@
 
 
 def car_fleet(target: int, position: list[int], speed: list[int]) -> int:
+    fleets = 0
+    stack = []
     
+    # iterate position and speed
     
+        # get length of time to target
+        
+        # while current length of time < last in the stack 
+        
+            # keep popping until stack is empty, add to fleet, then add current time to stack
+            
+        # add current length of time to stack
+        
+    # return fleets
+
 print(car_fleet(10, [1, 4], [3, 2])) # 1
 print(car_fleet(10, [4, 1, 0, 7], [2, 2, 1, 1])) # 3
