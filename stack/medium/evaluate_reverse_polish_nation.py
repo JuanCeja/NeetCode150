383800b73c79e1828f9073632ac29d647215b5d1
JuanCeja <jmc347ls2@yahoo.com>
2026-01-01 18:55:27 -0800

    refactor previous working solution for better readibility

--- stack/medium/evaluate_reverse_polish_nation.py
+++ stack/medium/evaluate_reverse_polish_nation.py
@@ -2,29 +2,26 @@
 # 
 # 
 
-def evaluate_reverse_polish_nation(tokens: list[str]) -> int:
-
+def evaluate_reverse_polish_notation(tokens: list[str]) -> int:
     stack = []
-    operators = {"+", "-", "*", "/"}
-
+    
     for token in tokens:
-
-        if token in operators:
-            num_1 = stack.pop()
-            num_2 = stack.pop()
-                
-            if token == "+":
-                stack.append(num_1 + num_2)
-            if token == "-":
-                stack.append(num_2 - num_1)
-            if token == "*":
-                stack.append(num_1 * num_2)
-            if token == "/":
-                stack.append(int(num_2 / num_1))
+        if token == "+":
+            stack.append(stack.pop() + stack.pop())
+        elif token == "-":
+            right = stack.pop()
+            left = stack.pop()
+            stack.append(left - right)
+        elif token == "*":
+            stack.append(stack.pop() * stack.pop())
+        elif token == "/":
+            right = stack.pop()
+            left = stack.pop()
+            stack.append(int(left / right))
         else:
             stack.append(int(token))
-            
+    
     return stack[0]
 
-# print(evaluate_reverse_polish_nation(["1","2","+","3","*","4","-"])) # 5
-print(evaluate_reverse_polish_nation(["10", "6", "9", "3", "/", "-11", "*", "/", "*", "17", "+", "5", "+"])) #22
+print(evaluate_reverse_polish_notation(["1","2","+","3","*","4","-"])) # 5
+print(evaluate_reverse_polish_notation(["10", "6", "9", "3", "/", "-11", "*", "/", "*", "17", "+", "5", "+"])) #22
