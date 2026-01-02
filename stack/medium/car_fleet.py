9e051c2b5725fea33b64610558756bc5c22a78bb
JuanCeja <jmc347ls2@yahoo.com>
2026-01-01 22:44:45 -0800

    initial commit for car fleet algorithm

---
+++ stack/medium/car_fleet.py
@@ -0,0 +1,10 @@
+# 
+# 
+# 
+
+
+def car_fleet(target: int, position: list[int], speed: list[int]) -> int:
+    
+    
+print(car_fleet(10, [1, 4], [3, 2])) # 1
+print(car_fleet(10, [4, 1, 0, 7], [2, 2, 1, 1])) # 3
