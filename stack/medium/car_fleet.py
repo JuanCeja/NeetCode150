8581063d86bea01842c58555f71a5732b8635eb9
JuanCeja <jmc347ls2@yahoo.com>
2026-01-05 19:34:41 -0800

    create first attempt to solve car fleet algorithm

--- stack/medium/car_fleet.py
+++ stack/medium/car_fleet.py
@@ -8,16 +8,22 @@ def car_fleet(target: int, position: list[int], speed: list[int]) -> int:
     stack = []
     
     # iterate position and speed
+    for i in range(len(position)):
     
         # get length of time to target
+        time_to_target = target / speed[i]
         
         # while current length of time < last in the stack 
-        
+        while stack and time_to_target < stack[-1]:
             # keep popping until stack is empty, add to fleet, then add current time to stack
-            
+            while stack:
+                stack.pop()
+            fleet += 1
         # add current length of time to stack
-        
+        stack.append(time_to_target)
     # return fleets
+    return fleets
+
 
 print(car_fleet(10, [1, 4], [3, 2])) # 1
 print(car_fleet(10, [4, 1, 0, 7], [2, 2, 1, 1])) # 3
