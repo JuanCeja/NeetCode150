b3508bcfe0cce44562c4a563ab00f4dfcf52e4c9
Juan Ceja MBP <jmc347ls2@yahoo.com>
2026-01-08 12:27:42 -0800

    create plan / pseudo code for Search for a 2D Matrix

--- Binary_Search/easy/search_a_2D_matrix.py
+++ Binary_Search/easy/search_a_2D_matrix.py
@@ -5,6 +5,22 @@
 # 
 
 search_matrix(matrix: list[list[int]], target: int) -> bool:
+    # use binary search in the outer arr
+
+        # check if target is in between first and last num of our mid array
+
+            # if so, use binary search in that array
+
+                # if result is not found return false
+
+        # if target > then last value in mid array
+
+            # left = mid + 1
+
+        # else 
+
+            # right = mid
+
 
 
 print(search_matrix([[1,2,4,8],[10,11,12,13],[14,20,30,40]], 10)) # true
