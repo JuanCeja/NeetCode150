73561d77f2de22bae643f033d994ec12c18a19ed
Juan Ceja MBP <jmc347ls2@yahoo.com>
2026-01-08 12:20:02 -0800

    initial commit for search a 2d matrix algorithm

---
+++ Binary_Search/easy/search_a_2D_matrix.py
@@ -0,0 +1,11 @@
+# 
+# 
+# 
+# 
+# 
+
+search_matrix(matrix: list[list[int]], target: int) -> bool:
+
+
+print(search_matrix([[1,2,4,8],[10,11,12,13],[14,20,30,40]], 10)) # true
+print(search_matrix([[1,2,4,8],[10,11,12,13],[14,20,30,40]], 15)) # false
