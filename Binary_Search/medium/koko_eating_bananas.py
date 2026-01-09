ceae86b92b14c63ae015a6bcaf833d1d47b0d421
JuanCeja <jmc347ls2@yahoo.com>
2026-01-08 19:06:36 -0800

    create efficient solution with O(nlogm) time complexity

--- Binary_Search/medium/koko_eating_bananas.py
+++ Binary_Search/medium/koko_eating_bananas.py
@@ -4,30 +4,26 @@
 # 
 # 
 
+import math
+
+
 def min_eating_speed(piles: list[int], h: int) -> int:
-    # sort piles
-    # get max of piles
-    # rate = h
     
-    # binary search in sorted piles
+    left, right = 1, max(piles)
     
-        # calc mid
-        # sum of hours
+    while left < right:
+        mid = (right + left) // 2
+        sum_of_hours = 0
         
-        # iterate sort piles and get sum
-        
-        # update h
-        
-        # if sum < h
-        
-            # right = mid
+        for i in piles:
+            sum_of_hours += math.ceil(i / mid)
+
+        if sum_of_hours <= h:
+            right = mid
+        else:
+            left = mid + 1
             
-        # else
-        
-            # left = mid + 1
-            
-    # return rate
-    
+    return left
 
 print(min_eating_speed([1,4,3,2], 9)) # 2
 print(min_eating_speed([25,10,23,4], 4)) # 25
