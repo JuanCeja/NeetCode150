50d203bd15bb9d43f41d671100580d54dd50b350
JuanCeja <jmc347ls2@yahoo.com>
2026-01-08 20:26:00 -0800

    initial commit for find minimum in rotated sorted array

---
+++ Binary_Search/medium/find_minimum_in_rotated_sorted_array.py
@@ -0,0 +1,12 @@
+# 
+# 
+# 
+# 
+# 
+
+def find_min(nums: list[int]) -> int:
+    
+    
+
+print(find_min([3,4,5,6,1,2])) # 1
+print(find_min([4,5,0,1,2,3])) # 0
--- Binary_Search/medium/koko_eating_bananas.py
+++ Binary_Search/medium/koko_eating_bananas.py
@@ -10,20 +10,23 @@ import math
 def min_eating_speed(piles: list[int], h: int) -> int:
     
     left, right = 1, max(piles)
+    rate = max(piles)
     
     while left < right:
+    
         mid = (right + left) // 2
         sum_of_hours = 0
         
         for i in piles:
             sum_of_hours += math.ceil(i / mid)
-
+        
         if sum_of_hours <= h:
             right = mid
+            rate = mid
         else:
             left = mid + 1
             
-    return left
+    return rate
 
 print(min_eating_speed([1,4,3,2], 9)) # 2
 print(min_eating_speed([25,10,23,4], 4)) # 25
