1d0763acdc64cecff49000b51872b7631d7dbb8a
JuanCeja <jmc347ls2@yahoo.com>
2026-01-08 18:22:25 -0800

    create plan / pseudo code for koko eating bananas algorithm

--- Binary_Search/medium/koko_eating_bananas.py
+++ Binary_Search/medium/koko_eating_bananas.py
@@ -5,7 +5,28 @@
 # 
 
 def min_eating_speed(piles: list[int], h: int) -> int:
+    # sort piles
+    # get max of piles
+    # rate = h
     
+    # binary search in sorted piles
+    
+        # calc mid
+        # sum of hours
+        
+        # iterate sort piles and get sum
+        
+        # update h
+        
+        # if sum < h
+        
+            # right = mid
+            
+        # else
+        
+            # left = mid + 1
+            
+    # return rate
     
 
 print(min_eating_speed([1,4,3,2], 9)) # 2
