5356abe2f56a4168dab1588e9beae855857676e5
Juan Ceja MBP <jmc347ls2@yahoo.com>
2026-01-12 14:23:15 -0800

    create plan / pseudo code for solution in O( log n) for Find Minimum in Rotated Sorted Arrray algorithm

--- Binary_Search/medium/find_minimum_in_rotated_sorted_array.py
+++ Binary_Search/medium/find_minimum_in_rotated_sorted_array.py
@@ -5,8 +5,27 @@
 # 
 
 def find_min(nums: list[int]) -> int:
-    
-    
+
+# l, r
+# min_num
+
+# while l < r
+
+    # if nums l > nums r 
+
+        # if mid > nums right
+
+            # left = mid
+
+        # else 
+
+            # min_num = mid or min_num
+
+            # right = mid
+
+    # return min_num
+
 
 print(find_min([3,4,5,6,1,2])) # 1
 print(find_min([4,5,0,1,2,3])) # 0
+# [9 1 2 3 4 5]
