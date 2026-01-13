dc91698c28a4c12ad95968ea281d86d2be58ca29
JuanCeja <jmc347ls2@yahoo.com>
2026-01-12 20:38:24 -0800

    complete solution in O(log n) for Find Minimum in Rotated Array algorithm

--- Binary_Search/medium/find_minimum_in_rotated_sorted_array.py
+++ Binary_Search/medium/find_minimum_in_rotated_sorted_array.py
@@ -6,24 +6,22 @@
 
 def find_min(nums: list[int]) -> int:
 
-# l, r
-# min_num
-
-# while l < r
-
-    # if nums l > nums r 
-
-        # if mid > nums right
-
-            # left = mid
-
-        # else 
-
-            # min_num = mid or min_num
-
-            # right = mid
-
-    # return min_num
+    l, r = 0, len(nums) - 1
+    min_num = float("inf")
+    
+    if nums[l] < nums[r] or len(nums) < 2:
+        return nums[0]
+    
+    while l <= r:
+        mid = (l + r) // 2
+        min_num = min(min_num, nums[mid])
+        
+        if nums[mid] <= nums[r]:
+            r = mid - 1
+        else:
+            l = mid + 1
+            
+    return min_num
 
 
 print(find_min([3,4,5,6,1,2])) # 1
