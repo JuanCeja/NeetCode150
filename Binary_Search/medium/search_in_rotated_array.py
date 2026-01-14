dd4620715514b42039385a38fed2e1dbca0d27ce
JuanCeja <jmc347ls2@yahoo.com>
2026-01-13 16:54:24 -0800

    first attempt for Search In Rotated Array

--- Binary_Search/medium/search_in_rotated_array.py
+++ Binary_Search/medium/search_in_rotated_array.py
@@ -8,20 +8,21 @@
 
 def search(nums: list[int], target: int) -> int:
     
-    # l, r
+    l, r = 0, len(nums) - 1
     
-    # while l <= r
-    
-        # mid
+    while l <= r:
+        mid = (l + r) // 2
+        mid_value = nums[mid]
         
-        # if mid == target return mid
-        
-        # if mid < tar & nums[mid + 1] < 1 move left to mid
-        
-        # else right to mid
+        if mid_value == target:
+            return mid
 
-    # ret fal
+        if mid_value < target and nums[mid + 1] < mid_value:
+            l = mid + 1
+        else: 
+            r = mid - 1
 
+    return -1
 
 print(search([3,4,5,6,1,2], 1)) # 4
 print(search([3,5,6,0,1,2], 4)) # -1
