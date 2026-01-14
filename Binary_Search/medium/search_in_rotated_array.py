3d1273a8a059f39d6e5f634a2aabbcf80097f600
JuanCeja <jmc347ls2@yahoo.com>
2026-01-13 17:46:57 -0800

    second attemp at Search in Rotated Array algorithm

--- Binary_Search/medium/search_in_rotated_array.py
+++ Binary_Search/medium/search_in_rotated_array.py
@@ -13,14 +13,20 @@ def search(nums: list[int], target: int) -> int:
     while l <= r:
         mid = (l + r) // 2
         mid_value = nums[mid]
-        
+
         if mid_value == target:
             return mid
 
-        if mid_value < target and nums[mid + 1] < mid_value:
-            l = mid + 1
-        else: 
-            r = mid - 1
+        if nums[l] <= mid_value:
+            if nums[l] <= target < mid_value:
+                r = mid - 1
+            else:
+                l = mid + 1
+        else:
+            if mid_value <= target < nums[r]:
+                l = mid + 1
+            else:
+                r = mid - 1
 
     return -1
 
