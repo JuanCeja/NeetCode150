014641d272b592dd6481b2a9c4b30d3ee03efdbf
JuanCeja <jmc347ls2@yahoo.com>
2026-01-13 20:15:24 -0800

    create a second attempt for Time Based Key Value Store algorithm

--- Binary_Search/medium/time_based_key_value_store.py
+++ Binary_Search/medium/time_based_key_value_store.py
@@ -1,36 +1,30 @@
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-
-
 class TimeMap:
 
     def __init__(self):
-        self.store = {
-            self.key = (self.value, self.timestamp)
-        }
+        self.store = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store:
+            self.store[key] = []
         self.store[key].append((value, timestamp))
     
 
     def get(self, key: str, timestamp: int) -> str:
         l, r = 0, len(self.store[key]) - 1
+        closest = 0
 
-        while l < r:
+        while l <= r:
             mid = (l + r) // 2
-            mid_value = self.store[key][-1]
+            mid_timestamp = self.store[key][mid][-1]
 
-            if mid_value == timestamp:
-                return self.store[key][0]
+            if mid_timestamp == timestamp:
+                return self.store[key][mid][0]
 
-            if mid_value < timestamp:
-                r = mid - 1
-            else:
+            if mid_timestamp <= timestamp:
+                closest = max(closest, mid)
                 l = mid + 1
+            else:
+                r = mid - 1
 
-        return 
+
+        return self.store[key][closest][0]
