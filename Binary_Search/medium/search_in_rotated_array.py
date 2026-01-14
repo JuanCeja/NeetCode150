f5b8e9ef5a520618734279ed196694398af37a9d
JuanCeja <jmc347ls2@yahoo.com>
2026-01-13 16:45:28 -0800

    plan / pseudo code for search in rotated array algorithm

--- Binary_Search/medium/search_in_rotated_array.py
+++ Binary_Search/medium/search_in_rotated_array.py
@@ -8,7 +8,20 @@
 
 def search(nums: list[int], target: int) -> int:
     
+    # l, r
     
+    # while l <= r
+    
+        # mid
+        
+        # if mid == target return mid
+        
+        # if mid < tar & nums[mid + 1] < 1 move left to mid
+        
+        # else right to mid
+
+    # ret fal
+
 
 print(search([3,4,5,6,1,2], 1)) # 4
 print(search([3,5,6,0,1,2], 4)) # -1
