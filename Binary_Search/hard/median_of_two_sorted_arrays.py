b48974a53cec3978ed5bfd1ac7004701375c1c9f
JuanCeja <jmc347ls2@yahoo.com>
2026-01-14 19:21:04 -0800

    initial commit for Median of Two Sorted Arrays algorithm

---
+++ Binary_Search/hard/median_of_two_sorted_arrays.py
@@ -0,0 +1,14 @@
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+
+def find_median_sorted_arrays(nums1: list[int], nums2: list[int]) -> bool:
+    
+    
+
+print(find_median_sorted_arrays([1,2], [3])) # 2.0
+print(find_median_sorted_arrays([1,3], [2,4])) # 2.5
