47df8e23d022307271c2d05303c5cb3c4b8954d0
JuanCeja <jmc347ls2@yahoo.com>
2026-01-27 20:52:50 -0800

    checkpoint: complete the reverse list function that we will need for our solution

--- Linked_List/medium/reorder_linked_list.py
+++ Linked_List/medium/reorder_linked_list.py
@@ -13,4 +13,26 @@ class ListNode:
 
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
+        slow = head
+        fast = head
         
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+        mid = slow
+        
+    def reverse_list(head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        prev = ListNode()
+        curr = head
+
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+            
+        return prev.next
+    
+    # im working on getting my reverse linked list to work
