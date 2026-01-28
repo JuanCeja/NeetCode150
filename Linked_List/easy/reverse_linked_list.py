908547c797f5dd8fccc74990f769dbe3ab39b291
JuanCeja <jmc347ls2@yahoo.com>
2026-01-27 16:56:10 -0800

    first attempt at reverse linked list algo

--- Linked_List/easy/reverse_linked_list.py
+++ Linked_List/easy/reverse_linked_list.py
@@ -17,4 +17,14 @@ from typing import Optional
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = head
+        forward = head.next
+        head.next = None
         
+        while forward:
+            fast_forward = forward.next
+            forward.next = prev
+            prev = forward
+            forward = fast_forward
+            
+        return head
