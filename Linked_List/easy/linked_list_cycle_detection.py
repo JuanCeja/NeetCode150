217d1cd42a542fa0d69ae491ab59440252773b4d
JuanCeja <jmc347ls2@yahoo.com>
2026-01-27 20:27:54 -0800

    complete optimized solution for linked list cycle detection using Floyd's cycle detection algorithm

--- Linked_List/easy/linked_list_cycle_detection.py
+++ Linked_List/easy/linked_list_cycle_detection.py
@@ -16,4 +16,14 @@ class ListNode:
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
         
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+            if fast == slow:
+                return True
+            
+        return False
