96cbceff66258fd51f2e8aec4f9d371ba4f9b65a
JuanCeja <jmc347ls2@yahoo.com>
2026-02-02 16:30:54 -0800

    first attempt at solution

--- Linked_List/medium/copy_linked_list_with_random_pointer.py
+++ Linked_List/medium/copy_linked_list_with_random_pointer.py
@@ -10,4 +10,20 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: Optional[Node]) -> Optional[Node]:
+        curr = head
+        dummy = Node(0, head, 0)
+        map = {}
         
+        while curr:
+            map[curr] = Node(curr.val, curr.next, curr.random)
+            curr = curr.next
+            
+        curr = head
+        
+        while curr:
+            dummy.val = map[curr].val
+            dummy.next = map[curr].next
+            dummy.random = map[curr].random
+            curr = curr.next
+        
+        return dummy
