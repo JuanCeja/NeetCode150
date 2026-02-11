103cefee1dac6da917e80a74a6d6db0d3e928ad6
JuanCeja <jmc347ls2@yahoo.com>
2026-02-10 17:23:15 -0800

    first attempt to add two numbers algorithm

--- Linked_List/medium/add_two_numbers.py
+++ Linked_List/medium/add_two_numbers.py
@@ -8,3 +8,34 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        curr_1, curr_2 = l1, l2
+        carry_over = False
+        product = ListNode()
+        
+        while (curr_1 is not None or curr_2 is not None) and (curr_1.next and curr_2.next):
+            sum = curr_1.val + curr_2.val
+            
+            if carry_over == True:
+                sum += 1
+                
+            if sum > 10:
+                sum = sum % 10
+                carry_over = True
+            
+            product.val = sum
+            carry_over = False
+            product.next = ListNode
+            curr_1 = curr_1.next
+            curr_2 = curr_2.next
+        
+        while curr_1.next is not None:
+            product.val = curr_1.val
+            product.next = ListNode()
+            curr_1 = curr_1.next
+
+        while curr_2.next is not None:
+            product.val = curr_2.val
+            product.next = ListNode()
+            curr_2 = curr_2.next
+            
+        return product
