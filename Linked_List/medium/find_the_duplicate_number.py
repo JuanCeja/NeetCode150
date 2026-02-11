cd2db7594d8a9817ae846f89bb28c7a25e018e7e
Juan Ceja MBP <jmc347ls2@yahoo.com>
2026-02-11 14:44:32 -0800

    create attempt to solve find the duplicate number algorithm

--- Linked_List/medium/find_the_duplicate_number.py
+++ Linked_List/medium/find_the_duplicate_number.py
@@ -1,3 +1,17 @@
+from typing import List
+
+
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        
+        slow, fast = 0,0
+
+        while nums[fast]:
+            fast = nums[nums[fast]]
+            slow = nums[slow + 1]
+
+
+        if slow == fast:
+            return slow
+
+
+# nums = [1,2,3,2,2]
