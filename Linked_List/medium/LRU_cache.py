3fe7fcd403297698b0f49c8cfa9a18d5dd5519a8
JuanCeja <jmc347ls2@yahoo.com>
2026-02-11 18:49:47 -0800

    second attempt at LRU cache

--- Linked_List/medium/LRU_cache.py
+++ Linked_List/medium/LRU_cache.py
@@ -12,29 +12,44 @@ class LRUCache:
         self.left.next, self.right.prev = self.right, self.left
 
     def get(self, key: int) -> int:
-        if self.dic[key]:
+        if key in self.dic:
+            self.move_to_recent(self.dic[key])
             return self.dic[key].value
         else:
             return -1
 
     def put(self, key: int, value: int) -> None:
-        while len(self.dic) >= self.capacity:
-            self.remove()
-            self.insert(Node(key, int))
+        if key in self.dic:
+            self.dic[key].value = value
+            self.move_to_recent(self.dic[key])
+        
+        else:
+            node = Node(key, value)
+            while len(self.dic) >= self.capacity:
+                self.remove()
+        
+            self.insert(node)
     
-    def insert(self, node):
-        if len(self.dic) >= self.capacity:
-            self.remove()
-            
+    def insert(self, node: Node):
         prev = self.right.prev
         prev.next = node
         node.prev = prev
         node.next, self.right.prev = self.right, node
+        self.dic[node.key] = node
         
-    def remove(self, node):
+    def remove(self, node: Node):
         curr = self.left.next
         next = curr.next
         self.left.next = next
         next.prev = self.left
         curr.next, curr.prev = None, None
         del self.dic[node]
+        
+    def move_to_recent(self, node: Node):
+        prev = node.prev
+        next = node.next
+        last_node = self.right
+        
+        prev.next, next.prev = next, prev
+        last_node.next, node.prev = node, last_node
+        node.next, self.right.prev = self.right, node
