62c447cbee18d2ac434d68245076105a85d72e68
Juan Ceja MBP <jmc347ls2@yahoo.com>
2026-02-17 14:40:47 -0800

    create helper function

--- Linked_List/hard/merge_k_sorted_linked_list.py
+++ Linked_List/hard/merge_k_sorted_linked_list.py
@@ -8,4 +8,30 @@ class ListNode:
 
 class Solution:    
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        return dummy
         
+    def mergeSortedLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if not l1 or not l2:
+            return l1 or l2
+        
+        dummy = ListNode()
+        current = dummy
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+
+            current = current.next
+
+        if l1:
+            current.next = l1
+
+        if l2:
+            current.next = l2
+
+        return dummy.next
