f6f90c012c1b20293bcf15b7d54435027f7717be
Juan Ceja MBP <jmc347ls2@yahoo.com>
2026-02-18 14:34:05 -0800

    create reverse linked list helper function

--- Linked_List/hard/reverse_nodes_in_k_group.py
+++ Linked_List/hard/reverse_nodes_in_k_group.py
@@ -5,4 +5,15 @@ class ListNode:
 
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+
         
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        return prev
