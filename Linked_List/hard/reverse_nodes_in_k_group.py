b275c839db6db9f93c2e3b3d2b603615457bd3d1
Juan Ceja MBP <jmc347ls2@yahoo.com>
2026-02-18 15:07:28 -0800

    another attempt

--- Linked_List/hard/reverse_nodes_in_k_group.py
+++ Linked_List/hard/reverse_nodes_in_k_group.py
@@ -5,8 +5,18 @@ class ListNode:
 
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        
-        
+        curr = head
+
+        while curr:
+            if self.getKthNode(curr, k):
+                beginning_of_group = curr.next
+                end_of_group = curr
+                self.reverseList(curr)
+                beginning_of_group.next = end_of_group
+            
+        return curr
+
+
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev, curr = None, head
 
