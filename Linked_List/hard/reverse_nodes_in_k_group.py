8095ca1ec0043969ec36ced01f05b604bc42e3f2
Juan Ceja MBP <jmc347ls2@yahoo.com>
2026-02-18 14:45:34 -0800

    first attempt at solution

--- Linked_List/hard/reverse_nodes_in_k_group.py
+++ Linked_List/hard/reverse_nodes_in_k_group.py
@@ -5,6 +5,27 @@ class ListNode:
 
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        slow, fast, dummy = head, head, head
+        slow_counter, fast_counter = 0, 0
+
+        while slow and fast:
+            slow = slow.next
+            fast = fast.next.next
+            slow_counter += 1
+            fast_counter += 2
+
+        if (fast_counter - slow_counter) >= k:
+            end_of_first_half = slow
+            beggining_of_second_half = slow.next
+            end_of_second_half = fast
+
+            self.reverseList(end_of_first_half)
+            self.reverseList(end_of_second_half)
+
+            beggining_of_second_half.next = end_of_first_half
+
+        return dummy
+
 
         
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
