1f58bf388f5d707b19ebfaa5ea6ff10a695a7c6c
Juan Ceja MBP <jmc347ls2@yahoo.com>
2026-02-18 13:46:19 -0800

    create solution

--- Linked_List/hard/merge_k_sorted_linked_list.py
+++ Linked_List/hard/merge_k_sorted_linked_list.py
@@ -8,8 +8,25 @@ class ListNode:
 
 class Solution:    
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        dummy = ListNode(0)
-        return dummy
+        if lists is None or len(lists) == 0:
+            return None
+        
+
+        while len(lists) > 1:
+            merged_lists = []
+
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+
+                merged_lists.append(self.mergeSortedLists(l1, l2))
+
+            lists = merged_lists
+            return lists[0]
+
+
+
+        
         
     def mergeSortedLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         if not l1 or not l2:
