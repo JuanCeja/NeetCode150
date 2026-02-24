22d31c55b60f9872ccea00caf4d2f8dfbbeba105
JuanCeja <jmc347ls2@yahoo.com>
2026-02-23 16:20:41 -0800

    create working solution for maximum depth of binary tree

--- Trees/easy/maximum_depth_of_binary_tree.py
+++ Trees/easy/maximum_depth_of_binary_tree.py
@@ -7,4 +7,10 @@ class TreeNode:
 
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
         
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+            
+        return 1 + max(left_depth, right_depth)
