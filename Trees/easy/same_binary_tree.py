a082e822d67dee30ddf89300cb882b91647e20a1
JuanCeja <jmc347ls2@yahoo.com>
2026-02-25 17:19:18 -0800

    first attempt at solution

--- Trees/easy/same_binary_tree.py
+++ Trees/easy/same_binary_tree.py
@@ -7,4 +7,24 @@ class TreeNode:
 
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        is_same = 0
         
+        def dfs(node_p, node_q) -> int:
+            if not node:
+                return 0
+            
+            if node_p.left.val == node_q.left.val:
+                dfs(node_p.left)
+                dfs(node_q.left)
+                
+            if node_p.right.val == node_q.right.val:
+                dfs(node_p.right)
+                dfs(node_q.right)
+                
+            
+            else:
+                return 1
+            
+        dfs(p)
+        dfs(q)
+        return is_same == 0
