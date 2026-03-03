f5f274c45edc6a0216fe986e682a0759a8ac62ef
JuanCeja <jmc347ls2@yahoo.com>
2026-03-02 18:32:03 -0800

    create working solution

--- Trees/medium/binary_tree_right_side_view.py
+++ Trees/medium/binary_tree_right_side_view.py
@@ -5,6 +5,33 @@
 #         self.left = left
 #         self.right = right
 
+from collections import deque
+
+
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
         
+        result = []
+        queue = deque([root])
+        
+        while queue:
+            qLen = len(queue)
+            
+            for _ in range(qLen - 1):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                    
+            last_node = queue.popleft()
+            
+            if last_node.left:
+                queue.append(last_node.left)
+            if last_node.right:
+                queue.append(last_node.right)
+            result.append(last_node.val)
+            
+        return result
