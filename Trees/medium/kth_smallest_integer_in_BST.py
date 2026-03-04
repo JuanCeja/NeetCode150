1657b85a1703235ef3f241218b2aa8f4684a9d8b
JuanCeja <jmc347ls2@yahoo.com>
2026-03-03 18:14:14 -0800

    create working solution for kth smallest integer in BST algorithm

--- Trees/medium/kth_smallest_integer_in_BST.py
+++ Trees/medium/kth_smallest_integer_in_BST.py
@@ -7,4 +7,19 @@ class TreeNode:
 
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        values = []
         
+        def in_order_traversal(node: TreeNode) -> None:
+            if not node:
+                return None
+            
+            if node.left:
+                in_order_traversal(node.left)
+                
+            values.append(node.val)
+            
+            if node.right:
+                in_order_traversal(node.right)
+                
+        in_order_traversal(root)
+        return values[k - 1]
