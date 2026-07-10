4d39d0b52c3e44a138412d9f215ecdee57c31c80
Juan Ceja MBP <jmc347ls2@yahoo.com>
2026-07-10 11:38:46 -0700

    complete working solution

--- Arrays_and_Hashing/medium/longestConsecutiveSequence.py
+++ Arrays_and_Hashing/medium/longestConsecutiveSequence.py
@@ -14,7 +14,23 @@
 # Output: 7
 
 def longest_consecutive(nums: list[int]) -> int:
-    
+    nums_set = set(nums)
+    max_count = 0
+
+    for num in nums:
+        if num - 1 in nums_set:
+            continue
+
+        count = 1
+
+        while num + 1 in nums_set:
+            num += 1
+            count += 1
+
+        max_count = max(count, max_count)
+
+    return max_count
+
 
 print(longest_consecutive([2,20,4,10,3,4,5])) # 4
 print(longest_consecutive([0,3,2,5,4,6,1,1])) # 7
