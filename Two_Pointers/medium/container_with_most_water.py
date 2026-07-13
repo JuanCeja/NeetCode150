62cfa82f33b84991d14cc3130a9cf2e731f54b5b
Juan Ceja MBP <jmc347ls2@yahoo.com>
2026-07-13 10:11:34 -0700

    initial commit

--- Two_Pointers/medium/container_with_most_water.py
+++ Two_Pointers/medium/container_with_most_water.py
@@ -12,21 +12,7 @@
 # Output: 4
 
 def max_area(heights: list[int]) -> int:
-    max_area = 0
-    left, right = 0, len(heights) - 1
-
-    while left < right:
-        width = right - left
-
-        current_area = min(heights[left], heights[right]) * width
-        max_area = max(current_area, max_area)
-
-        if heights[left] < heights[right]:
-            left += 1
-        else:
-            right -= 1
-            
-    return max_area
+    
 
 print(max_area([1,7,2,5,4,7,3,6])) # 36
 print(max_area([2,2,2])) # 4
