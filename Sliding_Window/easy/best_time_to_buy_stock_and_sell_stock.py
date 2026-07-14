8427e81cb59b51fc9a1ec5c9292d4e68c4da01b3
JuanCeja <jmc347ls2@yahoo.com>
2026-07-13 18:45:57 -0700

    completed

--- Sliding_Window/easy/best_time_to_buy_stock_and_sell_stock.py
+++ Sliding_Window/easy/best_time_to_buy_stock_and_sell_stock.py
@@ -4,7 +4,19 @@
 # 
 
 def max_profit (prices: list[int]) -> int:
-    
+    l, r = 0, 1
+    max_profit = 0
+
+    while r < len(prices):
+        profit = prices[r] - prices[l]
+        max_profit = max(max_profit, profit)
+        
+        if prices[l] > prices[r]:
+            l = r
+        
+        r += 1
+
+    return max_profit
 
 print(max_profit([10,1,5,6,7,1])) # 6
 print(max_profit([10,8,7,5,2])) # 0
