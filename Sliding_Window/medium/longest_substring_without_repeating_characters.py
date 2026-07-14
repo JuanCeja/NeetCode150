485a1beb497fb4974297a934a447f6cda9d84d7c
JuanCeja <jmc347ls2@yahoo.com>
2026-07-13 18:47:15 -0700

    initial commit

--- Sliding_Window/medium/longest_substring_without_repeating_characters.py
+++ Sliding_Window/medium/longest_substring_without_repeating_characters.py
@@ -5,19 +5,7 @@
 # 
 
 def length_of_longest_substring(s: str) -> int:
-    char_set = set()
-    left = 0
-    res = 0
-
-    for r in range(len(s)):
-        while s[r] in char_set:
-            char_set.remove(s[left])
-            left += 1
-
-        char_set.add(s[r])
-        res = max(res, r - left + 1)
-
-    return res
+    
 
 print(length_of_longest_substring("zxyzxyz")) # 3
 print(length_of_longest_substring("xxxx")) # 1
