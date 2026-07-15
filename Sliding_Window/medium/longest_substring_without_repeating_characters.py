7327e2136c2cc97c58d732a0f2f81c4e3e75f689
JuanCeja <jmc347ls2@yahoo.com>
2026-07-14 17:49:14 -0700

    current not working solution

--- Sliding_Window/medium/longest_substring_without_repeating_characters.py
+++ Sliding_Window/medium/longest_substring_without_repeating_characters.py
@@ -5,26 +5,29 @@
 # 
 
 def length_of_longest_substring(s: str) -> int:
-    l, r = 0, 1
+    l,r = 0, 1
     seen = {}
     longest_substring = 1
-    r += 1
+    seen[s[l]] = 0
 
     while r < len(s):
-        if s[r] in seen:
-            if seen[s[r]] <= l:
+        if s[r] not in seen:
+            seen[s[r]] = r
+            r += 1
+            longest_substring = max(longest_substring, len(s[l:r]))
+        else:
+            if seen[s[r]] < l:
                 seen[s[r]] = r
+                r += 1
+                longest_substring = max(longest_substring, len(s[l:r]))
             else:
                 l = r
-            r += 1
-        else:
-            curr_count = len(s[l:r])
-            longest_substring = max(longest_substring, curr_count)
-            r += 1
-            seen[s[r]] = r
+                r += 1
 
     return longest_substring
 
 
+
 print(length_of_longest_substring("zxyzxyz")) # 3
 print(length_of_longest_substring("xxxx")) # 1
+print(length_of_longest_substring("aab")) # 2
