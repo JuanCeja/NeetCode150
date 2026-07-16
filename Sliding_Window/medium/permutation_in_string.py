694af5732cb81524995c58afeb168e510ef76381
JuanCeja <jmc347ls2@yahoo.com>
2026-07-15 20:31:51 -0700

    completed

--- Sliding_Window/medium/permutation_in_string.py
+++ Sliding_Window/medium/permutation_in_string.py
@@ -9,7 +9,30 @@
 
 
 def permutation_in_string(s1: str, s2: str) -> bool:
+    if len(s1) > len(s2):
+        return False
+
+    count1 = {}
+    count2 = {}
+
+    for i in range(len(s1)):
+        count1[s1[i]] = count1.get(s1[i], 0) + 1
+        count2[s2[i]] = count2.get(s2[i], 0) + 1
+
+    if count1 == count2:
+        return True
     
+    for r in range(len(s1), len(s2)):
+        count2[s2[r]] = count2.get(s2[r], 0) + 1
+        count2[s2[r - len(s1)]] -= 1
+
+        if count2[s2[r - len(s1)]] == 0:
+            del count2[s2[r - len(s1)]]
+
+        if count1 == count2:
+            return True
+    
+    return False
 
 print(permutation_in_string("abc", "lecabee")) # True
 print(permutation_in_string("abc", "lecaabee")) # False
