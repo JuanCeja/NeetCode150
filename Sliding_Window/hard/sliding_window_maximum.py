6366a904ec2071dec0cd1ab176018972e7363521
JuanCeja <jmc347ls2@yahoo.com>
2026-07-22 18:34:40 -0700

    complete solution in linear time

--- Sliding_Window/hard/sliding_window_maximum.py
+++ Sliding_Window/hard/sliding_window_maximum.py
@@ -10,6 +10,22 @@ from collections import deque
 
 
 def max_sliding_window(nums: list[int], k: int) -> list[int]:
-    
+    d = deque()
+    res = []
+
+    for r in range(len(nums)):
+
+        while d and nums[d[-1]] < nums[r]:
+            d.pop()
+        d.append(r)
+
+        if d[0] <= r - k:
+            d.popleft()
+
+        if r >= k -1:
+            res.append(nums[d[0]])
+
+    return res
+        
 
 print(max_sliding_window([1,2,1,0,4,2,6], 3)) # [2,2,4,4,6]
