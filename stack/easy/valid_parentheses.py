b70fd1a557e7a9d175c4d0e67c82657251821381
JuanCeja <jmc347ls2@yahoo.com>
2026-07-23 19:57:24 -0700

    create solution

--- stack/easy/valid_parentheses.py
+++ stack/easy/valid_parentheses.py
@@ -3,7 +3,25 @@
 # 
 
 def is_valid(s: str) -> bool:
-    
+
+    stack = []
+
+    close_to_open = {
+        "}":"{",
+        ")":"(",
+        "]":"["
+    }
+
+    for r in range(len(s)):
+        if s[r] in close_to_open:
+            if stack and stack[-1] != close_to_open[s[r]]:
+                return False
+            else:
+                stack.pop()
+        else:
+            stack.append(s[r])
+
+    return len(stack) == 0
 
 
 print(is_valid("[]")) # true
