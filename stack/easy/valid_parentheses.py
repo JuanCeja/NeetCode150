22e97aeb9821512d31754aa57e27d69be693212d
JuanCeja <jmc347ls2@yahoo.com>
2026-07-23 18:25:47 -0700

    initial commit

--- stack/easy/valid_parentheses.py
+++ stack/easy/valid_parentheses.py
@@ -3,22 +3,7 @@
 # 
 
 def is_valid(s: str) -> bool:
-    stack = []
-    close_to_open = {
-        ")": "(",
-        "}": "{",
-        "]": "["
-    }
     
-    for char in s:
-        if char in close_to_open:
-            if not stack or close_to_open[char] != stack[-1]:
-                return False
-            stack.pop()
-        else:
-            stack.append(char)
-        
-    return len(stack) == 0
 
 
 print(is_valid("[]")) # true
