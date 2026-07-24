8281acd7d5a3df6376be81c0d6cbbbeb230c6724
JuanCeja <jmc347ls2@yahoo.com>
2026-07-23 18:20:01 -0700

    complete solution

--- Sliding_Window/hard/sliding_window_maximum.py
+++ Sliding_Window/hard/sliding_window_maximum.py
@@ -14,18 +14,16 @@ def max_sliding_window(nums: list[int], k: int) -> list[int]:
     res = []
 
     for r in range(len(nums)):
-
-        while d and nums[d[-1]] < nums[r]:
+        while d and nums[r] > nums[d[-1]]:
             d.pop()
         d.append(r)
 
-        if d[0] <= r - k:
+        if d and d[0] <= r - k:
             d.popleft()
 
-        if r >= k -1:
+        if r >= k - 1:
             res.append(nums[d[0]])
 
     return res
-        
 
 print(max_sliding_window([1,2,1,0,4,2,6], 3)) # [2,2,4,4,6]
