81ab0ee3b3d4f5470c20067a15437f656d3f4198
JuanCeja <jmc347ls2@yahoo.com>
2026-07-25 13:25:39 -0700

    initial commit

--- stack/medium/daily_temperature.py
+++ stack/medium/daily_temperature.py
@@ -4,18 +4,7 @@
 
 
 def daily_temperature(temperatures: list[int]) -> list[int]:
-    result = [0] * len(temperatures)
-    stack = []
     
-    for i in range(len(temperatures)):
-        
-        while stack and temperatures[i] > temperatures[stack[-1]]:
-            popped_index = stack.pop()
-            result[popped_index] = i - popped_index
-            
-        stack.append(i)
-        
-    return result
 
 print(daily_temperature([30,38,30,36,35,40,28])) # [1,4,1,2,1,0,0]
 print(daily_temperature([22,21,20])) # [0,0,0]
