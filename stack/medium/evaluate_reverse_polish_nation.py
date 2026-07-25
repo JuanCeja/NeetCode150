0b0bb283b28c4f8e5e5385186c82b50d55f98056
JuanCeja <jmc347ls2@yahoo.com>
2026-07-25 12:53:00 -0700

    initial commit

--- stack/medium/evaluate_reverse_polish_nation.py
+++ stack/medium/evaluate_reverse_polish_nation.py
@@ -3,25 +3,7 @@
 # 
 
 def evaluate_reverse_polish_notation(tokens: list[str]) -> int:
-    stack = []
     
-    for token in tokens:
-        if token == "+":
-            stack.append(stack.pop() + stack.pop())
-        elif token == "-":
-            right = stack.pop()
-            left = stack.pop()
-            stack.append(left - right)
-        elif token == "*":
-            stack.append(stack.pop() * stack.pop())
-        elif token == "/":
-            right = stack.pop()
-            left = stack.pop()
-            stack.append(int(left / right))
-        else:
-            stack.append(int(token))
-    
-    return stack[0]
 
 print(evaluate_reverse_polish_notation(["1","2","+","3","*","4","-"])) # 5
 print(evaluate_reverse_polish_notation(["10", "6", "9", "3", "/", "-11", "*", "/", "*", "17", "+", "5", "+"])) #22
