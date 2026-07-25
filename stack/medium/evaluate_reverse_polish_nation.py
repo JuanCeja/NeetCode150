4b931fd63bf74be75aa7892542ec264bb986002b
JuanCeja <jmc347ls2@yahoo.com>
2026-07-25 13:17:33 -0700

    create complete efficient solution

--- stack/medium/evaluate_reverse_polish_nation.py
+++ stack/medium/evaluate_reverse_polish_nation.py
@@ -3,7 +3,31 @@
 # 
 
 def evaluate_reverse_polish_notation(tokens: list[str]) -> int:
-    
+    stack = []
+
+    for num in tokens:
+        if num in "+-*/":
+            if num == "+":
+                num_1 = stack.pop()
+                num_2 = stack.pop()
+                stack.append(num_1 + num_2)
+            if num == "-":
+                num_1 = stack.pop()
+                num_2 = stack.pop()
+                stack.append(num_2 - num_1)
+            if num == "*":
+                num_1 = stack.pop()
+                num_2 = stack.pop()
+                stack.append(num_1 * num_2)
+            if num == "/":
+                num_1 = stack.pop()
+                num_2 = stack.pop()
+                stack.append(int(num_2 / num_1))
+        else:
+            stack.append(int(num))
+
+    return stack.pop()
+
 
 print(evaluate_reverse_polish_notation(["1","2","+","3","*","4","-"])) # 5
 print(evaluate_reverse_polish_notation(["10", "6", "9", "3", "/", "-11", "*", "/", "*", "17", "+", "5", "+"])) #22
