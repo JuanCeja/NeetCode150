0c9764a51fa935c644c9176d0e0aaede6d99169b
JuanCeja <jmc347ls2@yahoo.com>
2026-07-28 19:42:48 -0700

    initial commit

--- Binary_Search/easy/binary_search.py
+++ Binary_Search/easy/binary_search.py
@@ -6,19 +6,7 @@
 
 
 def binary_search(nums: list[int], target: int) -> int:
-    left, right = 0, len(nums)
     
-    while left < right:
-        mid = (right + left) // 2
-        
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] > target:
-            right = mid
-        else:
-            left = mid + 1
-            
-    return -1
 
 print(binary_search([-1,0,2,4,6,8], 4)) # 3
 print(binary_search([-1,0,2,4,6,8], 3)) # -1
