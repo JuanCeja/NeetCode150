a371d97f92a5295a4e5e30661adf1f25938ac709
JuanCeja <jmc347ls2@yahoo.com>
2026-08-01 15:06:34 -0700

    completed solution

--- Binary_Search/medium/koko_eating_bananas.py
+++ Binary_Search/medium/koko_eating_bananas.py
@@ -8,7 +8,23 @@ import math
 
 
 def min_eating_speed(piles: list[int], h: int) -> int:
-    
+    l, r = 1, max(piles)
+    res = float("inf")
+
+    while l <= r:
+        mid = (l + r) // 2
+
+        total_time = 0
+        for pile in piles:
+            total_time += math.ceil(pile / mid)
+
+        if total_time > h:
+            l = mid + 1
+        else:
+            res = min(res, mid)
+            r = mid - 1
+        
+    return res
 
 print(min_eating_speed([1,4,3,2], 9)) # 2
 print(min_eating_speed([25,10,23,4], 4)) # 25
