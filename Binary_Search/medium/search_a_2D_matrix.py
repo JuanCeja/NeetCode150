8d3aa7eb8fe9093d4b208d46ed16fb34bc745643
JuanCeja <jmc347ls2@yahoo.com>
2026-07-31 19:39:37 -0700

    refactored solution

--- Binary_Search/medium/search_a_2D_matrix.py
+++ Binary_Search/medium/search_a_2D_matrix.py
@@ -5,29 +5,32 @@
 # 
 
 def search_matrix(matrix: list[list[int]], target: int) -> bool:
-    out_l, out_r = 0, len(matrix) - 1
+    top, bottom = 0, len(matrix) - 1
 
-    while out_l <= out_r:
-        outter_mid = (out_l + out_r) // 2
+    while top <= bottom:
+        mid_row = (top + bottom) // 2
 
-        if target <= matrix[outter_mid][-1] and target >= matrix[outter_mid][0]:
-            l, r = 0, len(matrix[outter_mid]) - 1
-
-            while l <= r:
-                inner_mid = (l + r) // 2
-                if target == matrix[outter_mid][inner_mid]:
-                    return True
-                elif target > matrix[outter_mid][inner_mid]:
-                    l = inner_mid + 1
-                else:
-                    r = inner_mid - 1
-
-            return False
-        
-        elif target > matrix[outter_mid][-1]:
-            out_l = outter_mid + 1
+        if target > matrix[mid_row][-1]:
+            top = mid_row + 1
+        elif target < matrix[mid_row][0]:
+            bottom = mid_row - 1
         else:
-            out_r = outter_mid - 1
+            break
+    else:
+        return False
+
+    row = matrix[mid_row]
+    left, right = 0, len(row) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if target == row[mid]:
+            return True
+        elif target > row[mid]:
+            left = mid + 1
+        else:
+            right = mid - 1
 
     return False
 
