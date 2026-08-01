f93723c0c0e35456f8e55ed260b54f469eaa54a0
JuanCeja <jmc347ls2@yahoo.com>
2026-08-01 15:54:10 -0700

    initial commit

--- Binary_Search/medium/find_minimum_in_rotated_sorted_array.py
+++ Binary_Search/medium/find_minimum_in_rotated_sorted_array.py
@@ -5,23 +5,7 @@
 # 
 
 def find_min(nums: list[int]) -> int:
-
-    l, r = 0, len(nums) - 1
-    min_num = float("inf")
     
-    if nums[l] < nums[r] or len(nums) < 2:
-        return nums[0]
-    
-    while l <= r:
-        mid = (l + r) // 2
-        min_num = min(min_num, nums[mid])
-        
-        if nums[mid] <= nums[r]:
-            r = mid - 1
-        else:
-            l = mid + 1
-            
-    return min_num
 
 
 print(find_min([3,4,5,6,1,2])) # 1
--- Binary_Search/medium/search_in_rotated_array.py
+++ Binary_Search/medium/search_in_rotated_array.py
@@ -7,8 +7,22 @@
 
 
 def search(nums: list[int], target: int) -> int:
-    
-    
+    l, r = 0, len(nums) - 1
+
+    while l < r:
+        mid = (l + r) // 2
+
+        if nums[l] > nums[r]:
+            l = mid + 1
+        else:
+            break
+
+    res = float("inf")
+
+    while l <= r:
+        mid = (l + r) // 2
+
+        
 
 print(search([3,4,5,6,1,2], 1)) # 4
 print(search([3,5,6,0,1,2], 4)) # -1
