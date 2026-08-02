033521e3aae6794d6b09f31a0ae3ddebe255f89f
JuanCeja <jmc347ls2@yahoo.com>
2026-08-02 09:43:14 -0700

    completed solution

--- Binary_Search/medium/find_minimum_in_rotated_sorted_array.py
+++ Binary_Search/medium/find_minimum_in_rotated_sorted_array.py
@@ -5,9 +5,21 @@
 # 
 
 def find_min(nums: list[int]) -> int:
-    
+    res = float("inf")
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        mid = (l + r) // 2
+
+        res = min(res, nums[mid])
+
+        if nums[mid] < nums[r]:
+            r = mid - 1
+        else:
+            l = mid + 1
+
+    return res
 
 
 print(find_min([3,4,5,6,1,2])) # 1
 print(find_min([4,5,0,1,2,3])) # 0
-# [9 1 2 3 4 5]
