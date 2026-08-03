a7535e74f8f75891b8bb2c4c8c21dd6bada01cac
Juan Ceja MBP <jmc347ls2@yahoo.com>
2026-08-03 11:15:56 -0700

    initial commit

--- Linked_List/easy/reverse_linked_list.py
+++ Linked_List/easy/reverse_linked_list.py
@@ -17,16 +17,4 @@ from typing import Optional
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return head
         
-        prev = None
-        curr = head
-        
-        while curr:
-            next_node = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next_node
-
-        return prev
