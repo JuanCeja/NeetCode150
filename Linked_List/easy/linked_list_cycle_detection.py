a76a2d5f468b568986a3a62267353171b2ae0678
Juan Ceja MBP <jmc347ls2@yahoo.com>
2026-08-03 13:13:58 -0700

    initial commit

--- Linked_List/easy/linked_list_cycle_detection.py
+++ Linked_List/easy/linked_list_cycle_detection.py
@@ -16,14 +16,4 @@ class ListNode:
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow = head
-        fast = head
         
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            
-            if fast == slow:
-                return True
-            
-        return False
