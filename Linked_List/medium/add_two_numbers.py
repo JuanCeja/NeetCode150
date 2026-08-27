ab2d4823f3fbe181b5e89a8c2d73e528dbfd7f21
JuanCeja <jmc347ls2@yahoo.com>
2026-08-26 18:20:49 -0700

    first attempt

--- Linked_List/medium/add_two_numbers.py
+++ Linked_List/medium/add_two_numbers.py
@@ -8,5 +8,18 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        
-        
+        dummy = Node()
+        curr1, curr2 = l1, l2
+        carry_over = 0
+
+        while curr1 or curr2 or carry_over > 0:
+            v1 = curr1.val if curr1.val else 0
+            v2 = curr2.val if curr2.val else 0
+            total = v1 + v2
+            carry_over = total // 10
+            new_node = ListNode(total % 10)
+            dummy.next = new_node
+            curr1 = curr1.next
+            curr2 = curr2.next
+
+        return dummy
